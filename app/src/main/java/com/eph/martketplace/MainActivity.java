69d73775dfef40dca3f7363985bc9627a715999c
Ephraim19 <machayoephraim@gmail.com>
2022-03-26 14:01:14 +0300

    Created a new activity Fashion and an intent to link it to the main page

--- app/src/main/java/com/eph/martketplace/MainActivity.java
+++ app/src/main/java/com/eph/martketplace/MainActivity.java
@@ -2,13 +2,43 @@ package com.eph.martketplace;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Toast;
+
+import com.eph.martketplace.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "marketplace home";
+    private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(binding.getRoot());
+
+        binding.FashionButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //Toast.makeText(getApplicationContext(),"Home Button",Toast.LENGTH_LONG).show();
+                Intent intent = new Intent(MainActivity.this,Fashion.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
+
+
+
+
+
+
+
+
+
+
