469baeba96dfec9e7d6797fa386f51729d3aa54c
Ephraim19 <machayoephraim@gmail.com>
2022-03-27 10:31:06 +0300

    Added a fashion array adapter

---
+++ app/src/main/java/com/eph/martketplace/FashionArrayAdapter.java
@@ -0,0 +1,35 @@
+package com.eph.martketplace;
+
+import android.content.Context;
+import android.widget.ArrayAdapter;
+
+public class FashionArrayAdapter extends ArrayAdapter {
+    private Context mContext;
+    private String[] mFashion;
+
+    public FashionArrayAdapter(Context mContext,String[] mFashion, int resource){
+        super(mContext,resource);
+        this.mContext = mContext;
+        this.mFashion = mFashion;
+    }
+    @Override
+    public Object getItem(int position){
+        String fashion = mFashion[position];
+        return fashion;
+    }
+    @Override
+    public int getCount() {
+        return mFashion.length;
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
