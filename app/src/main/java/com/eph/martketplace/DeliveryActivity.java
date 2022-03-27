5d13ab2cce5505b2eebfcea2871c38e373d7d098
Ephraim19 <machayoephraim@gmail.com>
2022-03-27 23:20:52 +0300

    Connected location activity to delivery activity

--- app/src/main/java/com/eph/martketplace/DeliveryActivity.java
+++ app/src/main/java/com/eph/martketplace/DeliveryActivity.java
@@ -2,13 +2,21 @@ package com.eph.martketplace;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import com.eph.martketplace.databinding.ActivityDeliveryBinding;
+import com.eph.martketplace.databinding.ActivityLocationBinding;
 
 public class DeliveryActivity extends AppCompatActivity {
-
+    private ActivityDeliveryBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_delivery);
+        //Intent intent = getIntent();
+        //String location = intent.getStringExtra("location");
+        //binding.textView4.setText("Ephraim");
     }
 }
--- app/src/main/java/com/eph/martketplace/LocationActivity.java
+++ app/src/main/java/com/eph/martketplace/LocationActivity.java
@@ -22,10 +22,26 @@ public class LocationActivity extends AppCompatActivity {
         binding.LocationButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                //String location = binding.LocationButton.getText().toString();
                 Toast.makeText(getApplicationContext(),"Submitting Location",Toast.LENGTH_LONG).show();
                 Intent intent = new Intent(LocationActivity.this,DeliveryActivity.class);
+                //intent.putExtra("location", location);
                 startActivity(intent);
             }
         });
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
