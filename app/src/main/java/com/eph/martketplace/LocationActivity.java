2f271919abe2403b317d165fdc1932e19e364c72
Ephraim19 <machayoephraim@gmail.com>
2022-03-27 22:19:10 +0300

    Connected fashion activity with location activity

--- app/src/main/java/com/eph/martketplace/LocationActivity.java
+++ app/src/main/java/com/eph/martketplace/LocationActivity.java
@@ -2,13 +2,30 @@ package com.eph.martketplace;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import com.eph.martketplace.databinding.ActivityLocationBinding;
+
+import java.util.IntSummaryStatistics;
 
 public class LocationActivity extends AppCompatActivity {
-
+    private ActivityLocationBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_location);
+        binding = ActivityLocationBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        binding.LocationButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getApplicationContext(),"Submitting Location",Toast.LENGTH_LONG).show();
+                Intent intent = new Intent(LocationActivity.this,DeliveryActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
