03bf22d11f191427f1daac10e768ad98d5fd70f1
Ephraim19 <machayoephraim@gmail.com>
2022-04-03 20:48:40 +0300

    Added tweet fields and getter methods

--- app/src/main/java/com/eph/martketplace/dataModels/TwitterData.java
+++ app/src/main/java/com/eph/martketplace/dataModels/TwitterData.java
@@ -1,4 +1,14 @@
 package com.eph.martketplace.dataModels;
 
 public class TwitterData {
+    private Integer id;
+    private String tweet;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public String getTweet() {
+        return tweet;
+    }
 }
