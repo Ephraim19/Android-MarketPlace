1fdf6fc4356f14a13f068f92a13ad15519301021
Ephraim19 <machayoephraim@gmail.com>
2022-04-09 11:35:22 +0300

    Made login activity the launcher activity and made intents to redirect to other activities

--- app/src/main/java/com/eph/martketplace/auth/Login.java
+++ app/src/main/java/com/eph/martketplace/auth/Login.java
@@ -2,15 +2,78 @@ package com.eph.martketplace.auth;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
+import com.eph.martketplace.MainActivity;
 import com.eph.martketplace.R;
+import com.eph.martketplace.databinding.ActivityLoginBinding;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class Login extends AppCompatActivity {
+    private FirebaseAuth mAuth;
+    private ActivityLoginBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        binding = ActivityLoginBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
+
+        // Initialize Firebase Auth
+        mAuth = FirebaseAuth.getInstance();
+    }
+    // Check if user is signed in
+    @Override
+    public void onStart() {
+        super.onStart();
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        updateUI(currentUser);
+    }
+
+    private void updateUI(FirebaseUser currentUser) {
+        if (currentUser != null){
+            Toast.makeText(getApplicationContext(),"Logging in",Toast.LENGTH_SHORT).show();
+            Intent intent = new Intent(Login.this, MainActivity.class);
+            startActivity(intent);
+        }
+
+        //Starting sgn up activity
+        binding.register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getApplicationContext(),"register",Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent(Login.this, Sign_Up.class);
+                startActivity(intent);
+            }
+        });
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- app/src/main/java/com/eph/martketplace/auth/Sign_Up.java
+++ app/src/main/java/com/eph/martketplace/auth/Sign_Up.java
@@ -5,12 +5,18 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.eph.martketplace.R;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class Sign_Up extends AppCompatActivity {
-
+    private FirebaseAuth mAuth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+
+        //initializing firebase auth
+        mAuth = FirebaseAuth.getInstance();
     }
+
+    //Checking to see if user is signed in
 }
